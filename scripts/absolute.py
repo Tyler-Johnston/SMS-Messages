cd6924ce226c86f6b400fa3ad0532332f7506e1d
Tyler Johnston <tyler@Tylers-Air.attlocal.net>
2022-05-10 00:23:32 -0500

    removed insult.sh and reworded README

--- scripts/absolute.py
+++ scripts/absolute.py
@@ -2,18 +2,10 @@ import os
 
 absolute_path = os.path.abspath("../src/main.py")
 
-# add the absolute path of the user's PC to insult.sh
-with open("insult.sh", "r") as file:
-    data = file.read()
-data = data.replace("absolute_path_to_main.py", absolute_path)
-with open("insult.sh", "w") as file:
-    file.write(data)
-
-# add the absolute path of the user's PC to api.sh
 with open("api.sh", "r") as file:
     data = file.read()
 data = data.replace("absolute_path_to_main.py", absolute_path)
 with open("api.sh", "w") as file:
     file.write(data)
 
-print(f"Finished adding the absolute path {absolute_path} to api.sh and insult.sh")
+print(f"Finished adding the absolute path {absolute_path} to api.sh")
--- scripts/insult.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-insult=$(curl "https://insult.mattbas.org/api/insult")
-
-python3 absolute_path_to_main.py "$1" c "$insult"
